7248e25b1305fb998c49a1cfc14ed240ce5b8ea8
Kenneth Yong Yu <10272464+yonyu@users.noreply.github.com>
2023-02-12 18:28:47 -0800

    Initial commit

---
+++ src/Clean.Architecture.Core/ProjectAggregate/Specifications/ContributorByIdSpec.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+using Clean.Architecture.Core.ContributorAggregate;
+
+namespace Clean.Architecture.Core.ProjectAggregate.Specifications;
+
+public class ContributorByIdSpec : Specification<Contributor>, ISingleResultSpecification
+{
+  public ContributorByIdSpec(int contributorId)
+  {
+    Query
+        .Where(contributor => contributor.Id == contributorId);
+  }
+}
